f3d92a034512aa1ce974280341d12914bc650701
Василь Дячук <58522978+diachyk1vasul@users.noreply.github.com>
2020-01-13 20:40:49 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var i = 0;
+for ( i = 0; i < 100; i++){
+     
+    return i;
+}
+ if ( i > 0) {
+     console.log(1);
+ } else  if (i === 0) {
+     console.log(1);
+ } else  if (i <= 0) {
+    console.log(1);
+} else{
+    console.log(1);
+}
