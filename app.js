20880cb64b47b51dad29f603aad7cb8d3c793ca4
Василь Дячук <58522978+diachyk1vasul@users.noreply.github.com>
2020-01-13 22:01:43 +0200

    company

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-var i = 0;
-for ( i = 0; i < 100; i++){
-     
-    return i;
-}
- if ( i > 0) {
-     console.log(1);
- } else  if (i === 0) {
-     console.log(1);
- } else  if (i <= 0) {
-    console.log(1);
-} else{
-    console.log(1);
+for (var i = 0; i <= 100; i++) {
+    if ( i % 3 === 0 && i % 5 === 0 ) {
+        document.write('Thirty five <br/>');
+    } else if (i%3 === 0) {
+        document.write("Three <br/>");
+    } else if (i%5 === 0){
+        document.write("Five <br/>")
+    } else{
+        document.write(i + '<br/>');
+    }
 }
