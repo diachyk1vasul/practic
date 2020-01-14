ea81ce788316395de2dea09797bd87797539fc9e
Василь Дячук <58522978+diachyk1vasul@users.noreply.github.com>
2020-01-14 20:33:09 +0200

    company1

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-for (var i = 0; i <= 100; i++) {
-    if ( i % 3 === 0 && i % 5 === 0 ) {
-        document.write('Thirty five <br/>');
-    } else if (i%3 === 0) {
-        document.write("Three <br/>");
-    } else if (i%5 === 0){
-        document.write("Five <br/>")
-    } else{
-        document.write(i + '<br/>');
+var inputContent = document.getElementById("display");
+var firstInput = document.getElementById('firstInput');
+var secondInput = document.getElementById('secondInput');
+
+function counter(firstNumber, secondNumber) {
+    for (var i = firstNumber; i <= secondNumber; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            inputContent.innerHTML += " <b>Three & Five </b> " + i + "<br>";
+        } else if (i % 3 === 0) {
+            inputContent.innerHTML += "<b>Three </b>" + i + "<br>";
+        } else if (i % 5 === 0) {
+            inputContent.innerHTML += " <b>Five </b>" + i + "<br>";
+        } else {
+            inputContent.innerHTML += i + "<br>";
+        } 
     }
+}
+function submitCounter() {
+    counter(firstInput.value, secondInput.value);
+
+    return false;
 }
